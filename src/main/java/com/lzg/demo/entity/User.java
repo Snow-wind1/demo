305e7ede9b6105da3747700d4b640d9a1cfe2b42
LZG <1430039135@qq.com>
2022-06-25 22:12:21 +0800

    版本4

---
+++ src/main/java/com/lzg/demo/entity/User.java
@@ -0,0 +1,11 @@
+package com.lzg.demo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class User {
+    private String userName;
+    private int age;
+}

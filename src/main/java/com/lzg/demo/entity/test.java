fc387e55ffbb89c54eff8ba44698f105784e8c3a
LZG <1430039135@qq.com>
2022-06-26 18:48:48 +0800

    hot-fix

--- src/main/java/com/lzg/demo/entity/test.java
+++ src/main/java/com/lzg/demo/entity/test.java
@@ -2,6 +2,7 @@ package com.lzg.demo.entity;
 
 public class test {
     public static void main(String[] args) {
-
+        System.out.println("123");
+        System.out.println("hot-fix");
     }
 }
